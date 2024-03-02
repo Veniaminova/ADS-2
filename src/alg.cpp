4099848ea7fdb1882f10d0bfd83652ea61821cb1
Анастасия <98629650+Veniaminova@users.noreply.github.com>
2024-03-02 14:16:54 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,14 +3,20 @@
 #include <cmath>
 
 double pown(double value, uint16_t n) {
-    return pow(value, n);
+    double res = 1.0;
+    for (uint64_t i = 0; i < n; i++) {
+        res *= value;
+    }
+    return res;
 }
 
 uint64_t fact(uint16_t n) {
-    if (n == 1)
+    if (n == 0) {
         return 1;
-    else
+    }
+    else {
         return n * fact(n - 1);
+    }
 }
 
 double calcItem(double x, uint16_t n) {
@@ -18,26 +24,25 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-    double res = 1;
-    for (int i = 1; i <= count; ++i) {
+    double res = 0.0;
+    for (uint16_t i = 0; i < count; i++) {
         res += calcItem(x, i);
     }
     return res;
 }
 
 double sinn(double x, uint16_t count) {
-    double res = 0;
-    for (uint64_t i = 1; i <= count; ++i) {
-        res += pown(-1, i - 1) * pow(x, 2 * i - 1) / fact(2 * i - 1);
+    double res = 0.0;
+    for (uint16_t i = 0; i < count; i++) {
+        res += (i % 2 == 0 ? 1 : -1) * calcItem(x, 2 * i + 1);
     }
     return res;
 }
 
-
 double cosn(double x, uint16_t count) {
-    double res = 0;
-    for (int i = 0; i < count; i++) {
-        res += pow(-1, i) * pow(x, 2 * i) / tgamma(2 * i + 1);
+    double res = 1;
+    for (uint64_t i = 2; i <= count; i++) {
+        res += calcItem(x, (2 * i) - 2) * pown((-1.0), i - 1);
     }
     return res;
 }
